b97a52d4dbd6268c4f9413e458940162946edd63
brycekinnear <bryce.kinnear@spigit.com>
2017-07-12 19:39:16 +0000

    Added request, can post ideas

--- spigitbot.js
+++ spigitbot.js
@@ -1,4 +1,6 @@
-var fs = require('fs');
+var request = require('request');
+
+fs = require('fs');
 var RtmClient = require('@slack/client').RtmClient;
 var WebClient = require('@slack/client').WebClient;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
@@ -15,23 +17,25 @@ listen();
 // logs into slack team
 function authenticate() {
     rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
-        let self = rtmStartData.self;
-        console.log(`Logged in as ${self.name} of team ${rtmStartData.team.name}`);
+		let self = rtmStartData.self;
+		console.log(`Logged in as ${self.name} of team ${rtmStartData.team.name}`);
     });
 }
 
 // listens for message and responds
 function listen() {
-	
+
+    var identifier = '!';
+
     rtm.on(RTM_EVENTS.MESSAGE, (message) => {
 		// set default response text
 		var responseText = "Default";
 		
-		var trimmedMessage = message.text.trim();
+		var trimmedMessage = message.text.trim(); // cut off extraneous white space to avoid interference
 		var command;
 
 		// checking to make sure message is for bot
-		if (trimmedMessage.charAt(0) !== "!") {
+		if (trimmedMessage.charAt(0) !== identifier) {
 			return;
 		}
 		
@@ -40,13 +44,20 @@ function listen() {
 			return;
 		}
 		
-		// isolate the command from ! and whitespace
-		command = trimmedMessage.substr(1, trimmedMessage.length + 1).trim();
-		
+		// isolate the command from ! and split args into array
+		var arg = trimmedMessage.substr(1, Math.max(trimmedMessage.indexOf(','), trimmedMessage.length)).split(',');
+
+		// trim whitespace from every arg
+		for (var i = 0; i < arg.length; i++) {
+			arg[i] = arg[i].trim();
+		}
+
+		var command = arg[0].toLowerCase();
+
 		// generate response text based on message received
-		switch(command.toLowerCase()) {
+		switch(command) {
 		case "help":
-			responseText = "These are some commands you can give me: \n!idea [title] : posts an idea with title \n!random idea : displays a random idea from your community";
+			responseText = "These are some commands you can give me: \n!idea [title]\t\t\t\t: posts an idea with title \n!random idea\t\t\t: displays a random idea from your community";
 			break;
 		case "welcome":
 			responseText = "Thank you!";
@@ -56,18 +67,28 @@ function listen() {
 			responseText = "Your wish is my command.";
 			break;
 		case "hi":
+		case "hey":
 		case "hello":
 		case "salutations":
 		case "greetings":
 		case "what\'s up?":
 			responseText = "Well hello there!";
 			break;
+		case "post":
+			postIdea(0);
+
+			responseText = `POSTING TO ${arg[1]}`;
+			break;
 		case "idea":
-			responseText = "Posting idea...";
+			var auth_code = "1c255dce513f8d685e615cb94e8bf9c5";
+			
+			postIdea(auth_code);
+
+			responseText = "Posting idea with default settings. \nTitle: Default Title \nDesc: Default description \nTags: Default tag";
 			break;
 		case "random idea":
 			responseText = "Viewing random idea...";
-		break;
+			break;
 		default:
 			responseText = `I\'m sorry; I don\'t understand \'${command}\'.`;
 			break;
@@ -78,7 +99,9 @@ function listen() {
 				{
 					"fallback": "Notibot Message",
 					"title": responseText,
-					"color": "#36a64f"
+					"footer_icon": "https://avatars.slack-edge.com/2017-07-07/209849865429_a68b0ea005d80030e515_48.png",
+					"color": "#36a64f",
+					"footer": "Spigitbot"
 					//"ts": 123456789
 					// not sure if timestamp is neccessarily needed here
 				}
@@ -93,4 +116,37 @@ function listen() {
 // initiates Slack RTM API
 function initiate() {
     rtm.start();
+}
+
+// posts idea to Spigit API
+function postIdea(auth_code, url="https://qa40.spigit.com/api/v1/communities/1780/ideas") {
+	console.log(`Attempting post to ${url}...\n`);
+
+	var data = JSON.stringify({
+		"title" : "Slack post",
+		"category_id" : 10781,
+		"tags" : "this, is a, tag",
+		"post_anonymously" : false,
+		"template_fields" : {
+	    		"Content" : "Hi from Spigitbot"
+		}
+	});
+	
+	var header = {
+		"Authorization": `Bearer ${auth_code}`,
+		"Content-Type": "application/json"
+	}
+
+	var options = {
+		url: url,
+		body: data,
+		headers: header
+	}
+	
+	request.post(options, function (error, response, body) {
+			console.log(body);
+			console.log("\n ----error---- \n");
+			console.log(error);
+		}
+	);
 }
